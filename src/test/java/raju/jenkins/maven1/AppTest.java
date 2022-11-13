bb59aa235a749c098163347b01ab16171d366397
bvrajugithub2016 <bvraju_sca@yahoo.com>
2022-11-13 18:06:58 +0530

    Update AppTest.java
    browser type launch is parameterized

--- src/test/java/raju/jenkins/maven1/AppTest.java
+++ src/test/java/raju/jenkins/maven1/AppTest.java
@@ -2,6 +2,8 @@ package raju.jenkins.maven1;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -12,14 +14,35 @@ public class AppTest
 	@Test
 	public void myStartBrowser(){
 		
-		WebDriverManager.chromedriver().setup();
+		String browserName =  System.getProperty("browser");
 		
-		WebDriver driver = new ChromeDriver();
+		WebDriver driver = null;
+		
+		switch(browserName){
+			case "Chrome":
+							WebDriverManager.chromedriver().setup();
+							driver = new ChromeDriver();
+							System.out.println("Launching Chrome Browser");
+							break;	
+			case "Firebox":
+							WebDriverManager.firefoxdriver().setup();
+							driver = new FirefoxDriver();
+							System.out.println("Launching Firefox Browser");
+							break;
+			case "Edge":
+							WebDriverManager.edgedriver().setup();
+							driver = new EdgeDriver();
+							System.out.println("Launching Edge Browser");
+							break;
+		}
+		
+		
 		driver.get("https://www.gitam.edu/");
+		
 		
 		String gitamTitle =  driver.getTitle();
 		boolean result =  gitamTitle.contains("University");
-		Assert.assertTrue(result, "Title does not contain the word University");
+		Assert.assertTrue(result, "Title doesn't contain the word University");
 		driver.quit();
 	}
 }
